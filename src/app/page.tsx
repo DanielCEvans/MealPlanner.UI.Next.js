86b301ec3be90641bbd7460035c3762e6fc11734
Daniel Evans <daniel.evans@dyedurham.com>
2024-10-26 14:46:12 +1100

    feat: sign up/login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+'use client'
+
 import styles from "./page.module.css";
+import { useState } from "react";
+
 
 export default function Home() {
+  const [username, setUsername] = useState('')
+  
+  const registerClick = () => {
+    console.log(`register user ${username}`);
+  }
+  
+  const authenticateClick = () => {
+    console.log(`authenticate user ${username}`);
+  }
+  
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value)
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <form>
+          <input type="text" placeholder="username" value={username} onChange={handleInputChange}></input>
+          <input type="button" value={"Register"} onClick={registerClick}></input>
+          <input type="button" value={"Authenticate"} onClick={authenticateClick}></input>
+        </form>
       </main>
     </div>
   );
