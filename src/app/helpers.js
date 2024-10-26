efb10e79aad67825e62ca35e635628312d54960f
Daniel Evans <daniel.evans@dyedurham.com>
2024-10-26 21:53:58 +1100

    feat: coerceToBase64Url helper function

--- src/app/helpers.js
+++ src/app/helpers.js
@@ -43,3 +43,35 @@ export function showErrorAlert(message, error) {
         //footer: '<a href>Why do I have this issue?</a>'
     })
 }
+
+export function coerceToBase64Url (thing) {
+    // Array or ArrayBuffer to Uint8Array
+    if (Array.isArray(thing)) {
+        thing = Uint8Array.from(thing);
+    }
+
+    if (thing instanceof ArrayBuffer) {
+        thing = new Uint8Array(thing);
+    }
+
+    // Uint8Array to base64
+    if (thing instanceof Uint8Array) {
+        var str = "";
+        var len = thing.byteLength;
+
+        for (var i = 0; i < len; i++) {
+            str += String.fromCharCode(thing[i]);
+        }
+        thing = window.btoa(str);
+    }
+
+    if (typeof thing !== "string") {
+        throw new Error("could not coerce to string");
+    }
+
+    // base64 to base64url
+    // NOTE: "=" at the end of challenge is optional, strip it off here
+    thing = thing.replace(/\+/g, "-").replace(/\//g, "_").replace(/=*$/g, "");
+
+    return thing;
+};
