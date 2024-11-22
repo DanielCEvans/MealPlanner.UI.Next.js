1db779902c99515633dfadedc6445490a74ca97b
Daniel Evans <daniel.evans@dyedurham.com>
2024-11-22 20:21:56 +1000

    feat: add bottom navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,33 @@
 'use-client'
 
-import { Button } from "@mui/material"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-export default function Home(){
-    return (
-        <div>
-            <Button variant="contained">Hello World</Button>
-        </div>
-    )
+export default function BottomAppBar() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
 }
